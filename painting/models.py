eb0439e5b97b88a4c4a73dadfe5b92fae50c6880
Tanakorn <korn@beethoven.local>
2012-07-28 21:36:16 +0700

    Add description for each model

--- painting/models.py
+++ painting/models.py
@@ -3,24 +3,42 @@ from django.db import models
 class Brand(models.Model):
   name = models.CharField(max_length=32)
 
+  def __unicode__(self):
+    return self.name
+
 class Category(models.Model):
   brand = models.ForeignKey(Brand)
   name = models.CharField(max_length=32)
+
+  def __unicode__(self):
+    return self.brand.name + ' ' + self.name
 
 class SubCategory(models.Model):
   category = models.ForeignKey(Category)
   name = models.CharField(max_length=32)
 
+  def __unicode__(self):
+    return self.category.brand.name + ' ' + self.category.name + ' ' + self.name
+
 class Size(models.Model):
   category = models.ForeignKey(Category)
   value = models.CharField(max_length=32)
 
+  def __unicode__(self):
+    return self.category.brand.name + ' ' + self.category.name + ' ' + self.value
+
 class Base(models.Model):
   category = models.ForeignKey(Category)
-  value = models.CharField(max_length=16)
+  name = models.CharField(max_length=16)
+
+  def __unicode__(self):
+    return self.category.brand.name + ' ' + self.category.name + ' ' + self.name
 
 class Customer(models.Model):
   name = models.CharField(max_length=128)
+
+  def __unicode__(self):
+    return self.name
 
 class Sell(models.Model):
   subCategory = models.ForeignKey(SubCategory)
@@ -31,3 +49,4 @@ class Sell(models.Model):
   price = models.FloatField()
   customer = models.ForeignKey(Customer, null=True)
   note = models.TextField()
+  date = models.DateTimeField('Date')
