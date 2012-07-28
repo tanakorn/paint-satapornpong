13b2ec2034d6dcabc7aea4bdf2e44fb70841e3c0
Tanakorn <korn@beethoven.local>
2012-07-28 17:39:57 +0700

    Add paiting models to admin app

---
+++ painting/admin.py
@@ -0,0 +1,10 @@
+from painting.models import Brand, Category, SubCategory, Size, Base, Customer, Sell
+from django.contrib import admin
+
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Size)
+admin.site.register(Base)
+admin.site.register(Customer)
+admin.site.register(Sell)
