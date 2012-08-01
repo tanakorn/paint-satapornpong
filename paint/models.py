7887b931c837b856e3ef01a122c284644de15408
Tanakorn Leesatapornwongsa <tanakorn.lsv@gmail.com>
2012-08-01 19:56:42 +0700

    Alter models of paint by making paint item for each item

--- paint/models.py
+++ paint/models.py
@@ -14,25 +14,31 @@ class Product(models.Model):
     return self.brand.name + ' ' + self.name
 
 class Function(models.Model):
-  product = models.ForeignKey(Product)
   name = models.CharField(max_length=32)
 
   def __unicode__(self):
-    return self.product.brand.name + ' ' + self.product.name + ' ' + self.name
+    return self.name
 
 class Size(models.Model):
-  function = models.ForeignKey(Function)
   value = models.CharField(max_length=32)
 
   def __unicode__(self):
-    return self.product.brand.name + ' ' + self.product.name + ' ' + self.value
+    return self.value
 
 class Base(models.Model):
-  function = models.ForeignKey(Function)
   name = models.CharField(max_length=16)
 
   def __unicode__(self):
-    return self.product.brand.name + ' ' + self.product.name + ' ' + self.name
+    return self.name
+
+class Paint(models.Model):
+  product = models.ForeignKey(Product)
+  function = models.ForeignKey(Function)
+  size = models.ForeignKey(Size)
+  base = models.ForeignKey(Base)
+
+  def __unicode__(self):
+    return self.product.brand.name + ' ' + self.product.name + ' ' + self.function.name + ' ' + self.size.value + ' ' + self.base.name
 
 class Customer(models.Model):
   name = models.CharField(max_length=128)
@@ -41,10 +47,8 @@ class Customer(models.Model):
     return self.name
 
 class Sell(models.Model):
-  function = models.ForeignKey(Function)
-  size = models.ForeignKey(Size)
-  base = models.ForeignKey(Base)
-  code = models.CharField(max_length=8)
+  paint = models.ForeignKey(Paint)
+  code = models.CharField(max_length=16)
   unit = models.IntegerField()
   price = models.FloatField()
   customer = models.ForeignKey(Customer, null=True)
